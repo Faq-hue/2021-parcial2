7b314b3e388759bc2ad9cb0bcd77105a9a70281d
Ricardo Martín Marcucci <rmarku@gmail.com>
2021-10-13 22:36:29 -0300

    first commit

---
+++ src/info3/parcial2/Email.java
@@ -0,0 +1,14 @@
+package info3.parcial2;
+
+/**
+ * Clase con los datos de un email. Puede tener mas propiedades o metodos
+ */
+class Email {
+
+    public long id;
+    public String from;
+    public String to;
+    public String date;
+    public String subject;
+    public String content;
+}
