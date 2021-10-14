b98628b15cf476325bba87c7cd26528d22c72824
angelo59930 <manfrediangelosantiago10@gmail.com>
2021-10-14 09:12:44 -0300

    prueba

--- src/info3/parcial2/Main.java
+++ src/info3/parcial2/Main.java
@@ -2,8 +2,8 @@ package info3.parcial2;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// Implemetación
-        System.out.println("Parcial 2");
-    }
+  public static void main(String[] args) {
+    
+    System.out.println("prueba");
+  }
 }
