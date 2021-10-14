7d491ec1d4cfddec75652ef466c40308e33f2495
Facu <fzamora994@alumnos.iua.edu.ar>
2021-10-14 17:09:26 -0300

    Menu añadido

--- src/info3/parcial2/Main.java
+++ src/info3/parcial2/Main.java
@@ -7,6 +7,7 @@ import info3.parcial2.util.MailReader;
 public class Main {
 
   public static void main(String[] args) {
+
     List<Email> lista = new ArrayList<>();
     try {
       lista = MailReader.fileReader("src/info3/parcial2/mails/mails-20.txt");
